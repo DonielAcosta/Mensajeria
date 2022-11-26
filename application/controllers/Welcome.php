152c406f0e8fb94792251ee0a54eee637abf9d5b
Doniel Acosta <donielacosta1995@gmail.com>
2022-11-25 20:32:29 -0400

    instalacion y configuracion de entorno

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,23 +3,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
+
 	public function index()
 	{
 		$this->load->view('welcome_message');
 	}
+
+//   <?php
+// defined('BASEPATH') OR exit('No direct script access allowed');
+// use Twilio\Rest\Client;
+
+// class Welcome extends CI_Controller {
+
+// 	public function index()
+// 	{
+// 		$data = ['phone' => '+919703132428', 'text' => 'Hello, CI'];
+// 		print_r($this->sendSMS($data));
+// 	}
+
+// 	protected function sendSMS($data) {
+//           // Your Account SID and Auth Token from twilio.com/console
+//             $sid = 'your_sid';
+//             $token = 'your_token';
+// 	    $client = new Client($sid, $token);
+			
+//             // Use the client to do fun stuff like send text messages!
+//              return $client->messages->create(
+//                 // the number you'd like to send the message to
+//                 $data['phone'],
+//                 array(
+//                     // A Twilio phone number you purchased at twilio.com/console
+//                     "from" => "+your Twilio number",
+//                     // the body of the text message you'd like to send
+//                     'body' => $data['text']
+//                 )
+//             );
+//     }
+// }
 }
